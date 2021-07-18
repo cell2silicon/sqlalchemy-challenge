fa4ba29e7d1ce0d5e8cd888b277795c89666d9de
cell2silicon <83265414+cell2silicon@users.noreply.github.com>
2021-07-18 11:36:50 +1000

    vs code

--- app.py
+++ app.py
@@ -1,58 +1,80 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
-# Dictionary of Justice League
+# Create Engine
+engine = create_engine("sqlite:///./Resources/hawaii.sqlite")
 
-app = Flask(__name__)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
 
-justice_league_members = [
-    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
-    {"superhero": "Batman", "real_name": "Bruce Wayne"},
-    {"superhero": "Cyborg", "real_name": "Victor Stone"},
-    {"superhero": "Flash", "real_name": "Barry Allen"},
-    {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
-    {"superhero": "Superman", "real_name": "Clark Kent:Kal-El"},
-    {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
-]
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 
-@app.route("/")
-def home():
-    return "Hi"
-
-
-@app.route("/normal")
-def normal():
-    return justice_league_members
-
-
-@app.route("/jsonified")
-def jsonified():
-    return jsonify(justice_league_members)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
 #################################################
 # Flask Setup
-
 #################################################
-# @TODO: Initialize your Flask app here
-# YOUR CODE GOES HERE
+app = Flask(__name__)
+
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to Climate Analysis for Hawaii!<br/>"
+        f"Available Routes are:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
 
-# @TODO: Complete the routes for your app here
-# YOUR CODE GOES HERE
+@app.route("/api/v1.0/precipitation")
+def percipitation():
+    
+    percipitation = session.query(Measurement.date,Measurement.prcp).all()
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-#     # @TODO: Create your app.run statement here
-#     # YOUR CODE GOES HERE
+    perc = {date: prcp for date, prcp in percipitation}
+    return jsonify(perc)    
 
-# app = Flask(__justice_league_members__)
-# @app.route("/")
-# def home():
-#     print("Server received request for 'Home' page...")
-#     return "Welcome to my 'Home' page!"
+
+@app.route("/api/v1.0/stations")
+def station():
+    station = session.query(Station.name).all()
+    return jsonify(station)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    tobs = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date >= last_year).filter(Measurement.station == "USC00519281").all()
+    return jsonify(tobs)
+
+
+@app.route("/api/v1.0/temp/<start>")
+def temp_start(start):
+    temp = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    return jsonify(temp[0])
+
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+def temp_end(start,end):
+    temp = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    return jsonify(temp[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
